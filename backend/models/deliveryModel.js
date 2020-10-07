e90bab24a090965b5aa15826a9ef136ee2b9a734
michaelt <michaeltrepository@gmail.com>
2020-10-07 20:23:05 +0300

    Ok

--- backend/models/deliveryModel.js
+++ backend/models/deliveryModel.js
@@ -1,21 +1,21 @@
 import mongoose from 'mongoose';
 
-/*
+
 const deliveryItemsSchema = new mongoose.Schema({
-  package: { type: mongoose.Schema.Types.ObjectId, ref: 'Package', required: true },
   name: {type: String},
   size: {type: String},
   cost: {type: Number},
-});*/
+  packageId: { type: mongoose.Schema.Types.ObjectId, ref: 'Package', required: true },
+});
 
 const deliverySchema = new mongoose.Schema(
   {
     description: { type: String },
     shippingCost: { type: Number},
-    // sender: { type: mongoose.Schema.Types.ObjectId, ref: 'Sender' },
-    // courier: { type: mongoose.Schema.Types.ObjectId, ref: 'Courier' },
-    //deliveryItems: [deliveryItemsSchema],
-    //deliveryAt: { type: Date },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'Sender', required: true },
+    courier: { type: mongoose.Schema.Types.ObjectId, ref: 'Courier', required: true },
+    deliveryItems: [deliveryItemsSchema],
+    deliveryAt: { type: Date },
   },
   {
     timestamps: true,
