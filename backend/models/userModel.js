09dda9987f75d414e6c32fd44246e95f40cf7e83
michaelt <michaeltrepository@gmail.com>
2020-10-07 11:18:51 +0300

    ok

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,7 +5,8 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false, required: true },
+    isAdmin: { type: Boolean, default: false, required: true},
+    role: { type: String, default: 'customer'},
   },
   {
     timestamps: true,
